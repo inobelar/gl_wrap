f3e1faf4de108d41056c68f7976ef08583dc62fe
inobelar <inobelar@gmail.com>
2023-04-03 15:34:40 +0300

    QuickFix: added missing 'gl::getExtensions()' in header file

--- include/gl_wrap/gl_extensions.hpp
+++ include/gl_wrap/gl_extensions.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 namespace gl {
 
 bool isExtensionSupported(const char *name);
+
+std::vector<std::string> getExtensions();
 
 } // namespace gl
